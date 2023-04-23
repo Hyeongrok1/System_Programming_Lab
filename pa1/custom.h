e139004dae9bff1d387e41501a772d5001da31f9
Hyeongrok1 <loggy0323@g.skku.edu>
2023-04-23 15:09:59 +0900

    pa1 case 2 complete

--- pa1/custom.h
+++ pa1/custom.h
@@ -1,10 +1,70 @@
 //[include할 다른 헤더 파일 명시]
+#ifndef STDLIB_H
+# define STDLIB_H
+#endif
+#include <stdlib.h>
 
 //매크로 선언
 #define MAX_INPUT 1024
-
+#define MAX_HEAP 500000
 //구조체, type 선언
 
+typedef enum { false, true } bool;
+
+typedef struct _Node {
+    int item;
+    struct _Node* next;
+} Node;
+
+typedef struct {
+    Node *head;
+    int len;
+} LinkedList;
+
+void InitList(LinkedList *plist) {
+    plist->head = (Node *) malloc(sizeof(Node));
+    plist->head->next = NULL;
+    plist->len = 0;
+}
+
+bool IsEmpty(LinkedList *plist) {
+    return plist->len == 0;
+}
+
+void Insert(LinkedList *plist, int pos, int item) {
+    Node *cur, *newNode;
+    if (pos < 0 || pos > plist->len) exit(1);
+
+    newNode = (Node *)malloc(sizeof(Node));
+    newNode->item = item;
+    newNode->next = NULL;
+
+    cur = plist->head;
+    for (int i = 0; i < pos; i++) cur = cur->next;
+
+    newNode->next = cur->next;
+    cur->next = newNode;
+    plist->len++;
+}
+
+void RemoveLast(LinkedList *plist) {
+    Node *cur, *temp;
+    if (IsEmpty(plist)) return;
+
+    cur = plist->head;
+    while (cur->next != NULL) {
+        cur = cur->next;
+    }
+
+    plist->len--;
+    free(cur);
+}
+
+void ClearList(LinkedList *plist) {
+    while (plist->head->next != NULL)
+        RemoveLast(plist);
+    free(plist->head);
+}
 
 //[전역 변수 선언]
 
--- pa1/pa1.c
+++ pa1/pa1.c
@@ -9,7 +9,8 @@
 
 int start_idx[500000] = {0,};
 int common_line[500000] = {0,};
-
+int idx = 0;
+int base_len = 0;
 int case_id(int *ptr_cnt, char *tmp_ptr[], char *word, int cnt) {
     int case_id = 1;
     int tmp_cnt = 0;
@@ -86,11 +87,11 @@ int BoyerMooreSearch(int offset, int *lines, char *text, char *pattern)
     }
 }
 
-int BoyerMooreSearch2(int offset, int *lines, char *text, char *pattern)
+int BoyerMooreSearch2(int offset, int *lines, char *text, char *pattern, int *object_list)
 {
     int text_length = length(text);
     int pattern_length = length(pattern);
- 
+    idx = 0;
     int badchar[256] = { -1, };     // 모든 배열을 -1로 초기화한다
     for(int i=0 ; i<pattern_length ; i++)
         badchar[(int) pattern[i]] = i;   // 값을 채운다
@@ -112,7 +113,7 @@ int BoyerMooreSearch2(int offset, int *lines, char *text, char *pattern)
  
         if(j < 0 && (text[s+pattern_length] == ' ' || text[s+pattern_length] == '\n') && (text[s-1] == ' ' || text[s-1] == '\n'))                   // 문자열이 일치하였을 때
         {
-            common_line[lines[s] + 1] = 1;
+            object_list[idx++] = lines[s] + 1;
             s += (s < text_length-pattern_length) ? pattern_length-badchar[(int) text[s+pattern_length]] : 1;
         }
         else
@@ -120,6 +121,33 @@ int BoyerMooreSearch2(int offset, int *lines, char *text, char *pattern)
             s += max(1, j-badchar[(int) text[s+j]]);
         }
     }
+}
+
+int *intersection(int *base_list, int *object_list) {
+    int object_len = idx;
+    int intersection[base_len];
+    int k = 0;
+    for (int i = 0; i < base_len; i++) 
+    {
+        for (int j = 0; j < object_len; j++) 
+        {
+            if (base_list[i] == object_list[j]) {
+                intersection[k++] = base_list[i];
+            }
+        }
+    }
+    if (k == 0) {
+        base_len = k;
+        return base_list;
+    }
+    int *new = (int *) malloc(sizeof(int) * k);
+    for (int i = 0; i < k; i++) {
+        new[i] = intersection[i];
+    }
+    base_len = k;
+    free(base_list);
+
+    return new;
 }
 
 int main(int argc, char *argv[])
@@ -131,6 +159,8 @@ int main(int argc, char *argv[])
     int ptr_cnt = 0;
     int exit_or_not = 1;
     int offset = 0;
+    int *base_list = NULL;
+    int *object_list = NULL;
     
     char buf[MAX_INPUT];
     char *tmp_ptr[512];
@@ -190,31 +220,50 @@ int main(int argc, char *argv[])
             write(STDOUT_FILENO, "\n", 1);
         }
         else if (id == 2) {
-            for (int i = 0; i < ptr_cnt; i++) {
-                close(fd);
-                if ((fd = open(argv[1], O_RDONLY)) < 0) {
-                    write(STDERR_FILENO, "open err", 9);
-                    exit(1);
-                }
-                offset = 0; //initially, the offset is 0
-                char *tmp = tmp_ptr[i];
-
-                int tmp_cnt = 0;  
-                while(read(fd, buf, MAX_INPUT) != 0) {  //repeat reading
-                    buf[MAX_INPUT] = '\0';
-                    for (int i = 0; i < MAX_INPUT; i++) {   //count the line
-                        if (buf[i] == '\n') {
-                            tmp_cnt++;      //if we meet \n, increase tmp_cnt by 1
-                            start_idx[tmp_cnt] = offset + i + 1;
-                        }
-
-                        lines[i] = tmp_cnt;                
+            close(fd);
+            if ((fd = open(argv[1], O_RDONLY)) < 0) {
+                write(STDERR_FILENO, "open err", 9);
+                exit(1);
+            }
+            offset = 0; //initially, the offset is 0
+            int base_lines[1024] = {0,};
+            int tmp_cnt = 0;  
+            while(read(fd, buf, MAX_INPUT) != 0) {  //repeat reading
+                base_list = (int*) malloc(sizeof(int) * 1024);
+                object_list = (int*) malloc(sizeof(int) * 1024);
+                buf[MAX_INPUT] = '\0';
+                for (int i = 0; i < MAX_INPUT; i++) {   //count the line
+                    if (buf[i] == '\n') {
+                        tmp_cnt++;      //if we meet \n, increase tmp_cnt by 1
+                        start_idx[tmp_cnt] = offset + i + 1;
                     }
 
-                    BoyerMooreSearch2(offset, lines, buf, tmp);
-                    offset += MAX_INPUT;
+                    lines[i] = tmp_cnt;                
                 }
+
+                for (int i = 0; i < ptr_cnt; i++) {
+                    char *tmp = tmp_ptr[i];
+                    if (i == 0) {
+                        BoyerMooreSearch2(offset, lines, buf, tmp, base_list);
+                        base_len = idx;
+                    }
+                    else {
+                        BoyerMooreSearch2(offset, lines, buf, tmp, object_list);
+                        base_list = intersection(base_list, object_list); 
+                    }
+                }
+                int tmp_line = -1;
+                for (int i = 0; i < base_len; i++) {
+                    if (base_list[i] > tmp_line) printf("%d ", base_list[i]);
+                    tmp_line = base_list[i];
+                }
+
+                offset += MAX_INPUT;
+                free(object_list);
+                free(base_list);
             }
+            fflush(stdout);
+
             write(STDOUT_FILENO, "\n", 1);
         }
         else if (id == 3) {
@@ -246,7 +295,7 @@ int main(int argc, char *argv[])
             write(STDOUT_FILENO, "\n", 1);
         }
         else if (id == 4) {
-
+            
         }
 
 
