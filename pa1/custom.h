4f019763f7a04b8d7d9736682200cd8ea718279c
Hyeongrok1 <loggy0323@g.skku.edu>
2023-04-23 18:52:11 +0900

    add

--- pa1/custom.h
+++ pa1/custom.h
@@ -5,66 +5,17 @@
 #include <stdlib.h>
 
 //매크로 선언
-#define MAX_INPUT 1024
+#define MAX_INPUT 4096
 #define MAX_HEAP 500000
 //구조체, type 선언
 
 typedef enum { false, true } bool;
 
-typedef struct _Node {
-    int item;
-    struct _Node* next;
-} Node;
+//[전역 변수 선언]
 
-typedef struct {
-    Node *head;
-    int len;
-} LinkedList;
+//[함수 선언]
 
-void InitList(LinkedList *plist) {
-    plist->head = (Node *) malloc(sizeof(Node));
-    plist->head->next = NULL;
-    plist->len = 0;
-}
 
-bool IsEmpty(LinkedList *plist) {
-    return plist->len == 0;
-}
-
-void Insert(LinkedList *plist, int pos, int item) {
-    Node *cur, *newNode;
-    if (pos < 0 || pos > plist->len) exit(1);
-
-    newNode = (Node *)malloc(sizeof(Node));
-    newNode->item = item;
-    newNode->next = NULL;
-
-    cur = plist->head;
-    for (int i = 0; i < pos; i++) cur = cur->next;
-
-    newNode->next = cur->next;
-    cur->next = newNode;
-    plist->len++;
-}
-
-void RemoveLast(LinkedList *plist) {
-    Node *cur, *temp;
-    if (IsEmpty(plist)) return;
-
-    cur = plist->head;
-    while (cur->next != NULL) {
-        cur = cur->next;
-    }
-
-    plist->len--;
-    free(cur);
-}
-
-void ClearList(LinkedList *plist) {
-    while (plist->head->next != NULL)
-        RemoveLast(plist);
-    free(plist->head);
-}
 
 //[전역 변수 선언]
 
@@ -83,3 +34,38 @@ int length(const char* str)
     for (cnt = 0; str[cnt] != '\0'; cnt++);
     return cnt;
 }
+
+void reverse(char s[])
+{
+    int i, j;
+    char c;
+
+    for (i = 0, j = length(s)-1; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}  
+
+void itoa(int src,char* dst) {
+   int temp = 100000000;
+   int index = 0;
+
+   if (src == 0) { 
+      dst[0] = '0';
+      dst[1] = '\0'; 
+      return; 
+   }
+
+   while(src % temp == src) {
+      temp /= 10;
+   }
+   while(temp >= 1) { 
+      dst[index] = src / temp + '0';
+      index++;
+      src -= (src/temp)*temp;
+      temp /= 10;
+   }
+   dst[index] = '\0'; 
+} 
+
--- pa1/pa1.c
+++ pa1/pa1.c
@@ -4,13 +4,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <stdio.h>
 #include "custom.h"
+#include <stdio.h>
 
 int start_idx[500000] = {0,};
 int common_line[500000] = {0,};
 int idx = 0;
 int base_len = 0;
+int first_len = 0;
+int second_len = 0;
 int case_id(int *ptr_cnt, char *tmp_ptr[], char *word, int cnt) {
     int case_id = 1;
     int tmp_cnt = 0;
@@ -59,8 +61,8 @@ int BoyerMooreSearch(int offset, int *lines, char *text, char *pattern)
         j = pattern_length-1;
         
         while(j >= 0) {
-            if (text[s+j] >= 'a') text[s+j] = text[s+j] - 'a' + 'A';
-            if (pattern[j] >= 'a') pattern[j] = pattern[j] - 'a' + 'A';
+            if (text[s+j] >= 65 && text[s+j] <= 90) text[s+j] += 32;
+            if (pattern[j] >= 65 && pattern[j] <= 90) pattern[j] += 32;
 
             if (text[s+j] == pattern[j]) j--;
             else break;
@@ -68,16 +70,16 @@ int BoyerMooreSearch(int offset, int *lines, char *text, char *pattern)
                                     // pattern의 문자와 text가 매칭하면 j를 줄여
                                     // 나쁜 문자를 찾는다
  
-        if(j < 0 && (text[s+pattern_length] == ' ' || text[s+pattern_length] == '\n') && (text[s-1] == ' ' || text[s-1] == '\n'))                   // 문자열이 일치하였을 때
+        if(j < 0 && (text[s+pattern_length] == ' ' || text[s+pattern_length] == '\n') && (text[s-1] == ' ' || text[s-1] == '\n' || s-1 < 0))                   // 문자열이 일치하였을 때
         {
-            int tmp = lines[s] + 1;
-            int tmp2 = offset + s - start_idx[lines[s]];
-            // write(STDOUT_FILENO, &tmp, sizeof(int));
-            // write(STDOUT_FILENO, ":", 1);
-            // write(STDOUT_FILENO, &tmp2, sizeof(int));
-            // write(STDOUT_FILENO, " ", 1);
-            printf("%d:%d ", tmp, tmp2);
-            fflush(stdout);
+            char tmp[10];
+            char tmp2[20];
+            itoa(lines[s] + 1, tmp);
+            itoa(offset + s - start_idx[lines[s]], tmp2);
+            write(STDOUT_FILENO, tmp, length(tmp));
+            write(STDOUT_FILENO, ":", 1);
+            write(STDOUT_FILENO, tmp2, length(tmp2));
+            write(STDOUT_FILENO, " ", 1);
             s += (s < text_length-pattern_length) ? pattern_length-badchar[(int) text[s+pattern_length]] : 1;
         }
         else
@@ -102,8 +104,8 @@ int BoyerMooreSearch2(int offset, int *lines, char *text, char *pattern, int *ob
         j = pattern_length-1;
  
         while(j >= 0) {
-            if (text[s+j] >= 'a') text[s+j] = text[s+j] - 'a' + 'A';
-            if (pattern[j] >= 'a') pattern[j] = pattern[j] - 'a' + 'A';
+            if (text[s+j] >= 65 && text[s+j] <= 90) text[s+j] += 32;
+            if (pattern[j] >= 65 && pattern[j] <= 90) pattern[j] += 32;
 
             if (text[s+j] == pattern[j]) j--;
             else break;
@@ -114,6 +116,43 @@ int BoyerMooreSearch2(int offset, int *lines, char *text, char *pattern, int *ob
         if(j < 0 && (text[s+pattern_length] == ' ' || text[s+pattern_length] == '\n') && (text[s-1] == ' ' || text[s-1] == '\n'))                   // 문자열이 일치하였을 때
         {
             object_list[idx++] = lines[s] + 1;
+            s += (s < text_length-pattern_length) ? pattern_length-badchar[(int) text[s+pattern_length]] : 1;
+        }
+        else
+        {
+            s += max(1, j-badchar[(int) text[s+j]]);
+        }
+    }
+}
+
+int BoyerMooreSearch3(int offset, int *lines, char *text, char *pattern, int *line_list, int *offset_list)
+{
+    int text_length = length(text);
+    int pattern_length = length(pattern);
+    idx = 0;
+    int badchar[256] = { -1, };     // 모든 배열을 -1로 초기화한다
+    for(int i=0 ; i<pattern_length ; i++)
+        badchar[(int) pattern[i]] = i;   // 값을 채운다
+ 
+    int s = 0, j;                   // s는 text에 대한 pattern의 이동값
+    while(s < text_length-pattern_length)
+    {
+        j = pattern_length-1;
+        
+        while(j >= 0) {
+            if (text[s+j] >= 65 && text[s+j] <= 90) text[s+j] += 32;
+            if (pattern[j] >= 65 && pattern[j] <= 90) pattern[j] += 32;
+
+            if (text[s+j] == pattern[j]) j--;
+            else break;
+        }
+                                    // pattern의 문자와 text가 매칭하면 j를 줄여
+                                    // 나쁜 문자를 찾는다
+ 
+        if(j < 0 && (text[s+pattern_length] == ' ' || text[s+pattern_length] == '\n') && (text[s-1] == ' ' || text[s-1] == '\n'))                   // 문자열이 일치하였을 때
+        {
+            line_list[idx] = lines[s] + 1;
+            offset_list[idx++] = offset + s - start_idx[lines[s]];
             s += (s < text_length-pattern_length) ? pattern_length-badchar[(int) text[s+pattern_length]] : 1;
         }
         else
@@ -150,6 +189,32 @@ int *intersection(int *base_list, int *object_list) {
     return new;
 }
 
+
+void *intersection4(int *first_list, int *first_offset_list, int *second_list, int *second_offset_list, int *base_list) {
+    int k = 0;
+    
+    for (int i = 0; i < first_len; i++) 
+    {
+        for (int j = 0; j < second_len; j++) 
+        {
+            if (first_list[i] == second_list[j] && second_offset_list[j] - first_offset_list[i] >= 3) {
+                base_list[k++] = first_list[i];
+            }
+        }
+    }
+    // if (k == 0) {
+    //     base_len = k;
+    //     return base_list;
+    // }
+    base_len = k;
+}
+
+void bufclean(char *buf) {
+    for (int i = 0; i < MAX_INPUT; i++) {
+        buf[i] = '\0';
+    }
+}
+
 int main(int argc, char *argv[])
 {
     int fd;     //file descriptor
@@ -161,12 +226,17 @@ int main(int argc, char *argv[])
     int offset = 0;
     int *base_list = NULL;
     int *object_list = NULL;
-    
+    int *first_list = NULL;
+    int *second_list = NULL;
+    int *first_offset_list = NULL;
+    int *second_offset_list = NULL;
+    int *base_offset_list = NULL;
+
     char buf[MAX_INPUT];
     char *tmp_ptr[512];
     char exit_keyword[] = "PA1EXIT";
     char *word = (char*) malloc(sizeof(char) * 512);
-
+    bufclean(buf);
     while (1) {
         char c;
         ptr_cnt = 0;
@@ -201,7 +271,6 @@ int main(int argc, char *argv[])
         int id = case_id(&ptr_cnt, tmp_ptr, word, cnt);
         if (id == 1) {  //when we should find only one word
             offset = 0; //initially, the offset is 0
-
             int tmp_cnt = 0;  
             while(read(fd, buf, MAX_INPUT) != 0) {  //repeat reading
                 buf[MAX_INPUT] = '\0';
@@ -216,10 +285,12 @@ int main(int argc, char *argv[])
 
                 BoyerMooreSearch(offset, lines, buf, word);
                 offset += MAX_INPUT;
+                bufclean(buf);
             }
             write(STDOUT_FILENO, "\n", 1);
         }
         else if (id == 2) {
+
             close(fd);
             if ((fd = open(argv[1], O_RDONLY)) < 0) {
                 write(STDERR_FILENO, "open err", 9);
@@ -254,15 +325,20 @@ int main(int argc, char *argv[])
                 }
                 int tmp_line = -1;
                 for (int i = 0; i < base_len; i++) {
-                    if (base_list[i] > tmp_line) printf("%d ", base_list[i]);
+                    if (base_list[i] > tmp_line) {
+                        char itoa_str[10];
+                        itoa(base_list[i], itoa_str);
+                        write(STDOUT_FILENO, itoa_str, length(itoa_str));
+                        write(STDOUT_FILENO, " ", 1);
+                    }
                     tmp_line = base_list[i];
                 }
 
                 offset += MAX_INPUT;
                 free(object_list);
                 free(base_list);
+                bufclean(buf);
             }
-            fflush(stdout);
 
             write(STDOUT_FILENO, "\n", 1);
         }
@@ -291,11 +367,54 @@ int main(int argc, char *argv[])
 
                 BoyerMooreSearch(offset, lines, buf, tmp);
                 offset += MAX_INPUT;
+                bufclean(buf);
             }
             write(STDOUT_FILENO, "\n", 1);
         }
         else if (id == 4) {
-            
+            char *second = tmp_ptr[0];
+            char *first = tmp_ptr[1];
+            offset = 0; //initially, the offset is 0
+
+            int tmp_cnt = 0; 
+            base_list = (int*) malloc(sizeof(int) * 1024);
+            first_list = (int*) malloc(sizeof(int) * 1024);
+            second_list = (int*) malloc(sizeof(int) * 1024); 
+            first_offset_list = (int*) malloc(sizeof(int) * 1024);
+            second_offset_list = (int*) malloc(sizeof(int) * 1024); 
+            while(read(fd, buf, MAX_INPUT) != 0) {  //repeat reading
+                buf[MAX_INPUT] = '\0';
+                for (int i = 0; i < MAX_INPUT; i++) {   //count the line
+                    if (buf[i] == '\n') {
+                        tmp_cnt++;      //if we meet \n, increase tmp_cnt by 1
+                        start_idx[tmp_cnt] = offset + i + 1;
+                    }
+
+                    lines[i] = tmp_cnt;                
+                }
+
+                BoyerMooreSearch3(offset, lines, buf, first, first_list, first_offset_list);
+                first_len = idx;
+                BoyerMooreSearch3(offset, lines, buf, second, second_list, second_offset_list);
+                second_len = idx;
+                intersection4(first_list, first_offset_list, second_list, second_offset_list, base_list); 
+                offset += MAX_INPUT;
+
+                for (int i = 0; i < base_len; i++) {
+                    char itoa_str[10];
+                    itoa(base_list[i], itoa_str);
+                    write(STDOUT_FILENO, itoa_str, length(itoa_str));
+                    write(STDOUT_FILENO, " ", 1);
+                    // printf("%d ", base_list[i]);
+                }
+                bufclean(buf);
+            }
+
+            free(first_list);
+            free(second_list);
+            free(first_offset_list);
+            free(second_offset_list);
+            write(STDOUT_FILENO, "\n", 1);
         }
 
 
