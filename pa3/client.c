37a95e892d903ca5081035b94924a8d31146753f
Hyeongrok1 <loggy0323@g.skku.edu>
2023-06-01 22:21:31 +0900

    p14

--- pa3/client.c
+++ pa3/client.c
@@ -3,21 +3,68 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define EXIT 0
+#define LOGIN 1
+#define RESERVE 2
+#define CHECK_RSV 3
+#define CANCEL_RSV 4
+#define LOG_OUT 5
 
 typedef struct _query {
-    int user;
-    int action;
-    int seat;
+    int user;       // user ID 0~1023
+    int action;     // action ID 1~5 (0 for termination)
+    int seat;       // seat
 } query;
 
 int main(int argc, char *argv[]) {
     int client_socket = socket(PF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_addr;
 
+    int fd = 0;
+    int nbytes = 0;
+    char buf[6*100];
+    char *queries[100];
+    int query_list[100][3] = {0,};
+    int query_num = 0;
     if (argc == 3) {
         /* Insert your code for terminal input */
     } else if (argc == 4) {
         /* Insert your code for file input */
+        if ((fd = open(argv[3], O_RDONLY)) < 0) {
+            perror("open");
+            exit(1);
+        }
+
+        while ((nbytes = read(fd, buf, sizeof(buf))) > 0) {
+            buf[nbytes] = '\0';
+            char *ptr = strtok(buf, "\n");
+            while (ptr != NULL) {
+                queries[query_num++] = ptr;
+                ptr = strtok(NULL, "\n");                
+            }
+            queries[query_num] = NULL;          
+        }
+
+        for (int i = 0; i < query_num; i++) {
+            int argc_tmp = 0;
+            char *ptr = strtok(queries[i], " ");
+            while (ptr != NULL) {
+                query_list[i][argc_tmp++] = atoi(ptr);
+                ptr = strtok(NULL, " ");
+            }
+            if (argc > 4) query_list[i][0] = -1;   // Invalid query
+        }
+
+        // lastly, exit query
+        query_list[query_num][0] = 0;
+        query_list[query_num][1] = 0;
+        query_list[query_num][2] = 0;
+
+        close(fd);
     } else {
         printf("Follow the input rule below\n");
         printf("==================================================================\n");
@@ -36,14 +83,84 @@ int main(int argc, char *argv[]) {
         printf("Connection failed\n");
         exit(1);
     }
-
+    
+    char *small_query_list[3];
+    char *query_line;
+    size_t size;
+    query_num = 0;
+    while (1) {
     /*
      * Insert your PA3 client code
      *
      * You should handle input query
      *
      * Follow the print format below
-     *
+     */
+        query new_query;  
+        if (argc == 3) {
+            
+            getline(&query_line, &size, stdin);   
+
+            int num = 0;
+            char *ptr = strtok(query_line, " ");
+            while (ptr != NULL) {
+                small_query_list[num++] = ptr;
+                ptr = strtok(NULL, " ");
+            }
+            if (num > 4) {
+                printf("Invalid query\n");
+                continue;
+            }
+
+            new_query.user = small_query_list[0];
+            new_query.action = small_query_list[1];
+            new_query.seat = small_query_list[2];
+        } else if (argc == 4) {  
+            new_query.user = query_list[query_num][0];
+            new_query.action = query_list[query_num][1];
+            new_query.seat = query_list[query_num][2];
+            query_num++;
+        }
+        int fd = 0
+        int num_bytes = 0;
+        memset(buffer, 0 1024);
+        
+        sprintf(buffer, "%d:%d:%d", new_query.user, new_query.action, new_query.seat);
+        write(client_socket, buffer, 1024);
+        memset(buffer, 0, 1024);
+        if ((num_bytes = read(client_socket, buffer, 1024)) <= 0) {
+            printf("read failed\n");
+            continue;
+        }
+        if (new_query.action == LOGIN) {
+            if (atoi(buffer) == 1) 
+                printf("Login success\n");
+            else if (atoi(buffer) == -1) 
+                printf("Login failed\n");        
+        } else if (new_query.action == RESERVE) {
+            int seat_reserve = atoi(buffer);
+            if (seat_reserve > 0) 
+                printf("Seat %d is reserved\n", seat_reserve);
+            else    
+                printf("Reservation failed\n");
+        } else if (new_query.action == CHECK_RSV) {
+
+        } else if (new_query.action == CANCEL_RSV) {
+            int seat_cancel = atoi(buffer);
+            if (seat_cancel > 0) 
+                printf("Seat %d is canceled\n", seat_cancel);
+            else 
+                printf("Cancel failed\n");
+        } else if (new_query.action == LOGOUT) {
+            if (atoi(buffer) == 1) 
+                printf("Logout success\n");
+            else 
+                printf("Logout failed\n");
+        } else if (new_query.action == EXIT) {
+
+        }
+        
+     /*
      * 1. Log in
      * - On success
      *   printf("Login success\n");
@@ -80,6 +197,13 @@ int main(int argc, char *argv[]) {
      *   printf("Logout failed\n");
      *
      */
+
+
+
+
+
+
+    }
 
     close(client_socket);
 
--- pa3/server.c
+++ pa3/server.c
@@ -2,18 +2,131 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#include <stdio.h>
+
+#define MAX_SEAT 256
+#define MAX_CLIENT 1024
+
+#define EXIT 0
+#define LOGIN 1
+#define RESERVE 2
+#define CHECK_RSV 3
+#define CANCEL_RSV 4
+#define LOGOUT 5
+
+pthread_mutex_t thread_num_lock = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct _query {
-    int user;
-    int action;
-    int data;
+    int user;   // user ID 0~1023
+    int action; // action ID 1~5 (0 for termination)
+    int data;   // seat
 } query;
+// If the data field is our of range, server returns -1
+
+int client_of_user[1024];
+int thread_num_flag[1024];
+int user_of_seat[256];
+
+/**
+ * 1. Log in  -data: passwd
+ * 2. Reserve -data: seat num
+ * 3. Check reservation -data: N/A
+ * 4. Cancel reservation -data: seat num
+ * 5. Log out -data: N/A
+*/
+
+void *client_thread(void *arg) {
+    int connected_fd = *((int*) arg);
+    int num_bytes = 0;
+    int fd = 0;
+    char buffer[512];
+    char return_buffer[512];
+    char *query_argv[3];
+    char *ptr = NULL;
+    int query_argc = 0;
+    int success = 0;
+    query new_query;
+    while (1) {
+        memset(buffer, 0, 512);
+        memset(return_buffer, 0, 512);
+
+        if ((num_bytes = read(connected_fd, buffer, 512)) > 0) {
+            buffer[num_bytes-1] = '\0';
+            
+            query_argc = 0;
+            ptr = strtok(buffer, ":");
+            while (ptr != NULL) { 
+                query_argv[query_argc++] = ptr;
+                ptr = strtok(NULL, ":");
+            }
+
+            new_query.user = atoi(query_argv[0]);
+            new_query.action = atoi(query_argv[1]);
+            new_query.data = atoi(query_argv[2]);
+            
+            // exit
+            if (new_query.action == EXIT) {
+
+            } else if (new_query.action == LOGIN) {
+                int passwd = new_query.data;
+
+                if (success) {
+                    strcpy(return_buffer, "1");
+                } else {
+                    strcpy(return_buffer, "-1");
+                }
+            } else if (new_query.action == RESERVE) {
+                int seat_num = new_query.data;
+
+                if (success) {
+                    strcpy(return_buffer, "seat_num");
+                } else {
+                    strcpy(return_buffer, "-1");
+                }
+            } else if (new_query.action == CHECK_RSV) {
+
+                if (success) {
+                    strcpy(return_buffer, "reservation seats");
+                    // use loop and sprintf
+                } else {
+                    strcpy(return_buffer, "-1");
+                }
+            } else if (new_query.action == CANCEL_RSV) {
+                int seat_num = new_query.data;
+
+                if (success) {
+                    strcpy(return_buffer, "seat num");
+                } else {
+                    strcpy(return_buffer, "-1");
+                }
+            } else if (new_query.action == LOGOUT) {
+                if (success) {
+                    strcpy(return_buffer, "1");
+                } else {
+                    strcpy(return_buffer, "-1");
+                }
+            }
+
+            write(connected_fd, return_buffer, 512);
+        }
+    }
+
+    pthread_exit(NULL);
+}
 
 int main(int argc, char* argv[])
 {
-    int serverSocket = socket(PF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in serverAddr;
 
+    if (argc < 2) {
+        printf("Received %d arguments. Please enter port number\n", argc-1);
+        exit(1);
+    }
+    int serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serverAddr, clientAddr;
+    pthread_t threads[1024];
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(atoi(argv[1]));
     serverAddr.sin_addr.s_addr = htons(INADDR_ANY);
@@ -31,6 +144,33 @@ int main(int argc, char* argv[])
      * and process query of client until get termination query
      *
      */
+    int connected_fd = 0;
+    int connected_address_length = 0;
+    int thread_num = 0;
+    int *send_val;
+    while(1) {
+        connected_address_length = sizeof(clientAddr);
+        if ((connected_fd = accept(serverSocket, (struct sockaddr *) &clientAddr, (socklen_t *) &connected_address_length)) < 0) {
+            printf("accept() failed.\n");
+            continue;
+        }
+
+        // lock
+        pthread_mutex_lock(&thread_num_lock);
+        for (int i = 0; i < 1024; i++) {
+            if (thread_num_flag[i] == 0) {
+                thread_num_flag[i] = 1;  
+                thread_num = i;
+            }
+        }
+        // unlock
+        pthread_mutex_unlock(&thread_num_lock);
+        send_val = &connected_fd;
+        if (pthread_create(&threads[thread_num], NULL, client_thread, (void *) send_val)) {
+            printf("pthread_create err\n");
+            continue;
+        }
+    }
 
     return 0;
 }
